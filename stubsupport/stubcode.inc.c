6a860e05d155613ae18d86149c19a3a8f52d093e
okuoku <mjt@cltn.org>
2024-08-04 02:55:12 +0900

    ABI: Use `union` for generic types
    To avoid type-aliasing

--- stubsupport/stubcode.inc.c
+++ stubsupport/stubcode.inc.c
@@ -24,13 +24,19 @@
 #include <stdint.h>
 #include <stdlib.h> /* Abort */
 
-typedef void* ptr;
-typedef uint64_t u64;
-typedef int64_t s64;
-typedef uint32_t u32;
-typedef int32_t s32;
-typedef float f32;
-typedef double f64;
+union nccv64_u {
+    void* ptr;
+    uintptr_t uptr;
+    intptr_t sptr;
+    uint64_t u64;
+    int64_t s64;
+    uint32_t u32;
+    int32_t s32;
+    float f32;
+    double f64;
+};
+
+typedef union nccv64_u nccv64;
 
 #define EXP_COMMA_arg ,
 #define EXP_COMMA_term
@@ -38,38 +44,33 @@ typedef double f64;
 #define STUB_EMIT_ARGS(sym) \
     ( SYM_ ## sym ## _ARG_EXPAND(ITR_EMIT_ARGS) )
 
-#define ITR_EMIT_ARGS(idx, pos, _) \
-    arg ## idx EXP_COMMA_ ## pos
-
-#define ITR_DECL_ARGS(idx,_,typ) \
-    const typ arg ## idx = *(typ *)&in[idx];
+#define ITR_EMIT_ARGS(idx, pos, typ) \
+    in[idx].typ EXP_COMMA_ ## pos
 
 #define STUB_RET_TYPE0(_,__,t) t
 #define STUB_RET_TYPE(sym) SYM_ ## sym ## _RET_EXPAND(STUB_RET_TYPE0)
 
 #define STUB_F0_void_void(sym) \
-    void stub_ ## sym(const uint64_t* in, uint64_t* out){ \
+    void stub_ ## sym(const nccv64* in, nccv64* out){ \
         (void)in; \
         (void)out; \
         sym(); \
     }
 
 #define STUB_F0_arg_void(sym) \
-    void stub_ ## sym(const uint64_t* in, uint64_t* out){ \
-        SYM_ ## sym ## _ARG_EXPAND(ITR_DECL_ARGS) \
+    void stub_ ## sym(const nccv64* in, nccv64* out){ \
         (void)out; \
         sym STUB_EMIT_ARGS(sym); \
     }
 
 #define STUB_F0_void_ret(sym) \
-    void stub_ ## sym(const uint64_t* in, uint64_t* out){ \
-        *(STUB_RET_TYPE(sym) *)out = sym(); \
+    void stub_ ## sym(const nccv64* in, nccv64* out){ \
+        out->STUB_RET_TYPE(sym) = sym(); \
     }
 
 #define STUB_F0_arg_ret(sym) \
-    void stub_ ## sym(const uint64_t* in, uint64_t* out){ \
-        SYM_ ## sym ## _ARG_EXPAND(ITR_DECL_ARGS) \
-        *(STUB_RET_TYPE(sym) *)out = sym STUB_EMIT_ARGS(sym); \
+    void stub_ ## sym(const nccv64* in, nccv64* out){ \
+        out->STUB_RET_TYPE(sym) = sym STUB_EMIT_ARGS(sym); \
     }
 
 #define ITR_F0_STUBS(x,y,sym) \
@@ -78,33 +79,33 @@ typedef double f64;
 FORWARD_0_STUB_EXPAND(ITR_F0_STUBS)
 
 static void 
-STUBFUNC(library_info)(const uint64_t* in, uint64_t* out){
-    out[0] = (uintptr_t) STUBNAME_STR;
-    out[1] = 0;
-    out[2] = FORWARD_0_STUB_COUNT;
-    out[3] = 0;
-    out[4] = 0;
-    out[5] = 0;
+STUBFUNC(library_info)(const nccv64* in, nccv64* out){
+    out[0].ptr = STUBNAME_STR;
+    out[1].u64 = 0;
+    out[2].s32 = FORWARD_0_STUB_COUNT;
+    out[3].u64 = 0;
+    out[4].u64 = 0;
+    out[5].u64 = 0;
 }
 
 #define ITR_EXPORT_INFO_F0(x,y,sym) \
     case SYM_ ## sym ## _IDX: \
-      out[0] = 0; \
-      out[1] = SYM_ ## sym ## _IDX; \
-      out[2] = (uintptr_t)#sym; \
-      out[3] = 0; /* FORWARD_0 */ \
-      out[4] = 0; /* Direct call */ \
-      out[5] = (uintptr_t)stub_ ## sym; \
-      out[6] = SYM_ ## sym ## _ARG_COUNT; \
-      out[7] = SYM_ ## sym ## _RET_COUNT; \
+      out[0].s32 = 0; \
+      out[1].s32 = SYM_ ## sym ## _IDX; \
+      out[2].ptr = #sym; \
+      out[3].s32 = 0; /* FORWARD_0 */ \
+      out[4].s32 = 0; /* Direct call */ \
+      out[5].ptr = stub_ ## sym; \
+      out[6].s32 = SYM_ ## sym ## _ARG_COUNT; \
+      out[7].s32 = SYM_ ## sym ## _RET_COUNT; \
       return;
 
 static void
-STUBFUNC(library_export_info)(const uint64_t* in, uint64_t* out){
-    switch(in[0] /* fnid */ ){
+STUBFUNC(library_export_info)(const nccv64* in, nccv64* out){
+    switch(in[0].s32 /* fnid */ ){
         FORWARD_0_STUB_EXPAND(ITR_EXPORT_INFO_F0)
         default:
-            out[0] = -1;
+            out[0].s32 = -1;
             return;
     }
 }
@@ -118,35 +119,35 @@ STUBFUNC(library_export_info)(const uint64_t* in, uint64_t* out){
 #define CITYPE_VALUE_ptr 6
 
 #define ITR_ARG_INFO_F0_ARG(offs,_,typ) \
-    out[3+offs] = CITYPE_VALUE_ ## typ;
+    out[3+offs].s32 = CITYPE_VALUE_ ## typ;
 
 #define ITR_ARG_INFO_F0_RET(offs,_,typ) \
-    out[3+offs+outoffs] = CITYPE_VALUE_ ## typ;
+    out[3+offs+outoffs].s32 = CITYPE_VALUE_ ## typ;
 
 #define ITR_ARG_INFO_F0(_,__,sym) \
     case SYM_ ## sym ## _IDX : \
       outoffs = SYM_ ## sym ## _ARG_COUNT; \
-      out[0] = 0; \
-      out[1] = SYM_ ## sym ## _ARG_COUNT; \
-      out[2] = SYM_ ## sym ## _RET_COUNT; \
+      out[0].s32 = 0; \
+      out[1].s32 = SYM_ ## sym ## _ARG_COUNT; \
+      out[2].s32 = SYM_ ## sym ## _RET_COUNT; \
       SYM_ ## sym ## _ARG_EXPAND(ITR_ARG_INFO_F0_ARG) \
       SYM_ ## sym ## _RET_EXPAND(ITR_ARG_INFO_F0_RET) \
       return;
 
 static void
-STUBFUNC(library_arg_info)(const uint64_t* in, uint64_t* out){
+STUBFUNC(library_arg_info)(const nccv64* in, nccv64* out){
     size_t outoffs;
-    switch(in[0] /* objid */){
+    switch(in[0].s32 /* objid */){
         FORWARD_0_STUB_EXPAND(ITR_ARG_INFO_F0)
         default:
-            out[0] = -1;
+            out->s32 = -1;
             return;
     }
 }
 
 NCCC_EXPORT void
-STUBFUNC(dispatch)(const uint64_t* in, uint64_t* out){
-    switch(in[0]){
+STUBFUNC(dispatch)(const nccv64* in, nccv64* out){
+    switch(in[0].s32){
         case 1:
             STUBFUNC(library_info)(NULL, out);
             return;
