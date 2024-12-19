95c4c04328790ceccb481b09e933a59592fdbc44
okuoku <mjt@cltn.org>
2024-12-19 23:52:33 +0900

    common: Migrate to `nccv64`

--- common/ncccdlfcn.c
+++ common/ncccdlfcn.c
@@ -6,25 +6,27 @@
 #include <dlfcn.h>
 #endif
 
+#include "nccv64.h"
+
 void
-dlfcn_open(const uint64_t* in, uint64_t* out){
+dlfcn_open(const nccv64* in, nccv64* out){
     // [path] => [res ptr]
     void* handle;
-    char* const path = (char*)in[0];
+    char* const path = in[0].ptr;
     handle = dlopen(path, RTLD_NOW);
     if(handle){
-        out[0] = 0;
+        out[0].s64 = 0;
     }else{
-        out[0] = -1;
+        out[0].s64 = -1;
     }
-    out[1] = (uintptr_t)handle;
+    out[1].uptr = (uintptr_t)handle;
 }
 
 void
-dlfcn_get(const uint64_t* in, uint64_t* out){
+dlfcn_get(const nccv64* in, nccv64* out){
     // [handle name] => [res ptr]
-    void* handle = (uintptr_t)in[0];
-    char* const symname = (uintptr_t)in[1];
+    void* handle = in[0].ptr;
+    char* const symname = in[1].ptr;
     void* ptr;
     char* errs;
 
@@ -32,11 +34,11 @@ dlfcn_get(const uint64_t* in, uint64_t* out){
     ptr = dlsym(handle, symname);
     errs = dlerror();
     if(!errs){
-        out[0] = 0;
-        out[1] = (uintptr_t)ptr;
+        out[0].s64 = 0;
+        out[1].ptr = ptr;
     }else{
-        out[0] = -1;
-        out[1] = -1;
+        out[0].s64 = -1;
+        out[1].s64 = -1;
     }
 }
 
--- common/ncccutils.c
+++ common/ncccutils.c
@@ -3,96 +3,98 @@
 
 #include <stdlib.h>
 
-typedef void (*nccc_call_t)(const uint64_t* in, uint64_t* out);
+#include "nccv64.h"
+
+typedef void (*nccc_call_t)(const nccv64* in, nccv64* out);
 
 UTILLIB_API void
-util_rawcall(const uint64_t* in, uint64_t* out){
+util_rawcall(const nccv64* in, nccv64* out){
     // [addr inbuf outbuf] => []
-    const nccc_call_t addr = (nccc_call_t)in[0];
-    const uint64_t* inbuf = (uintptr_t)in[1];
-    uint64_t* const outbuf = (uintptr_t)in[2];
+    const nccc_call_t addr = (nccc_call_t)in[0].ptr;
+    const nccv64* inbuf = in[1].ptr;
+    nccv64* const outbuf = in[2].ptr;
 
     addr(inbuf,outbuf);
 }
 
 UTILLIB_API void
-util_peek_u64(const uint64_t* in, uint64_t* out){
+util_peek_u64(const nccv64* in, nccv64* out){
     // [addr] => [v]
-    const uint64_t* addr = (uintptr_t)in[0];
+    const uint64_t* addr = in[0].ptr;
     const uint64_t d = *addr;
-    out[0] = d;
+    out[0].u64 = d;
 }
 
 UTILLIB_API void
-util_peek_u32(const uint64_t* in, uint64_t* out){
+util_peek_u32(const nccv64* in, nccv64* out){
     // [addr] => [v]
-    const uint32_t* addr = (uintptr_t)in[0];
+    const uint32_t* addr = in[0].ptr;
     const uint32_t d = *addr;
-    out[0] = d;
+    out[0].u32 = d;
 }
 
 UTILLIB_API void
-util_peek_u8(const uint64_t* in, uint64_t* out){
+util_peek_u8(const nccv64* in, nccv64* out){
     // [addr] => [v]
-    const uint8_t* addr = (uintptr_t)in[0];
+    const uint8_t* addr = in[0].ptr;
     const uint8_t d = *addr;
-    out[0] = d;
+    out[0].u8 = d;
 }
 
 UTILLIB_API void
-util_peek_ptr(const uint64_t* in, uint64_t* out){
+util_peek_ptr(const nccv64* in, nccv64* out){
     // [addr] => [v]
-    const uintptr_t* addr = (uintptr_t)in[0];
-    const uint64_t d = *addr;
-    out[0] = d;
+    const uintptr_t* addr = in[0].ptr;
+    const uintptr_t d = *addr;
+    out[0].uptr = d;
 }
 
 UTILLIB_API void
-util_poke_u64(const uint64_t* in, uint64_t* out){
+util_poke_u64(const nccv64* in, nccv64* out){
     // [addr v] => []
-    uint64_t* const dest = (uintptr_t)in[0];
+    uint64_t* const dest = in[0].ptr;
     (void) out;
-    *dest = in[1];
+    *dest = in[1].u64;
 }
 
 UTILLIB_API void
-util_poke_u32(const uint64_t* in, uint64_t* out){
+util_poke_u32(const nccv64* in, nccv64* out){
     // [addr v] => []
-    uint32_t* const dest = (uintptr_t)in[0];
+    uint32_t* const dest = in[0].ptr;
     (void) out;
-    *dest = in[1];
+    *dest = in[1].u32;
 }
 
 UTILLIB_API void
-util_malloc(const uint64_t* in, uint64_t* out){
+util_malloc(const nccv64* in, nccv64* out){
     // [size] => [addr]
-    const size_t size = in[0];
-    const void* addr = malloc(size);
+    const size_t size = in[0].u64;
+    void* addr = malloc(size);
     if(!addr){
         abort();
     }
-    out[0] = addr;
+    out[0].ptr = addr;
 }
 
 UTILLIB_API void
-util_free(const uint64_t* in, uint64_t* out){
+util_free(const nccv64* in, nccv64* out){
     // [addr] => []
-    const void* addr = (void*)(uintptr_t)in[0];
+    void* addr = in[0].ptr;
     free(addr);
 }
 
 UTILLIB_API void
-util_ptraddr(const uint64_t* in, uint64_t* out){
+util_ptraddr(const nccv64* in, nccv64* out){
     // [addr] => [addr]
-    out[0] = in[0];
+    out[0].u64 = in[0].u64;
 }
 
 UTILLIB_API void
-util_shortcircuit(const uint64_t* in, uint64_t* out){
+util_shortcircuit(const nccv64* in, nccv64* out){
     // Short-circuit call for wasm stub
     // [nccc-func-addr in] => dispatch
-    const nccc_call_t addr = (nccc_call_t)in[0];
-    const uint64_t* inbuf = (uintptr_t)in[1];
+    const nccc_call_t addr = (nccc_call_t)in[0].ptr;
+    const nccv64* inbuf = in[1].ptr;
 
     addr(inbuf,out);
 }
@@ -101,61 +103,61 @@ util_shortcircuit(const uint64_t* in, uint64_t* out){
 #include <math.h>
 
 UTILLIB_API void
-math_cos(const uint64_t* in, uint64_t* out){
+math_cos(const nccv64* in, nccv64* out){
     float x;
     float r;
-    x = *(float *)in;
+    x = in[0].f32;
     r = cosf(x);
-    *(float *)out = r;
+    out[0].f32 = r;
 }
 UTILLIB_API void
-math_sin(const uint64_t* in, uint64_t* out){
+math_sin(const nccv64* in, nccv64* out){
     float x;
     float r;
-    x = *(float *)in;
+    x = in[0].f32;
     r = sinf(x);
-    *(float *)out = r;
+    out[0].f32 = r;
 }
 UTILLIB_API void
-math_atan2(const uint64_t* in, uint64_t* out){
+math_atan2(const nccv64* in, nccv64* out){
     float x;
     float y;
     float r;
-    y = *(float *)&in[0];
-    x = *(float *)&in[1];
+    y = in[0].f32;
+    x = in[1].f32;
     r = atan2f(y,x);
-    *(float *)out = r;
+    out[0].f32 = r;
 }
 UTILLIB_API void
-math_sqrt(const uint64_t* in, uint64_t* out){
+math_sqrt(const nccv64* in, nccv64* out){
     float x;
     float r;
-    x = *(float *)in;
+    x = in[0].f32;
     r = sqrtf(x);
-    *(float *)out = r;
+    out[0].f32 = r;
 }
 UTILLIB_API void
-math_pow(const uint64_t* in, uint64_t* out){
+math_pow(const nccv64* in, nccv64* out){
     float b;
     float e;
     float r;
-    b = *(float *)&in[0];
-    e = *(float *)&in[1];
+    b = in[0].f32;
+    e = in[1].f32;
     r = powf(b,e);
-    *(float *)out = r;
+    out[0].f32 = r;
 }
 UTILLIB_API void
-math_fmodf(const uint64_t* in, uint64_t* out){
+math_fmodf(const nccv64* in, nccv64* out){
     // FIXME: https://cpprefjp.github.io/reference/cmath/fmod.html
     float x;
     float y;
     float r;
-    x = *(float *)&in[0];
-    y = *(float *)&in[1];
+    x = in[0].f32;
+    y = in[1].f32;
     r = remainder(fabs(x), (y = fabs(y)));
     if (signbit(r)) {
         r += y;
     }
     r = copysign(r, x);
-    *(float *)out = r;
+    out[0].f32 = r;
 }
